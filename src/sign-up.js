e24c04dd7dedd3e8f7084207f1993c8e2e7a4a00
joecosta117 <joecosta117@gmail.com>
2019-02-16 12:43:53 -0500

    created the basic form for the sign-up component

--- src/sign-up.js
+++ src/sign-up.js
@@ -9,4 +9,27 @@ class SignUp extends Component {
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
   }
+
+  handleChange(event) {
+    this.setState({email: event.target.value})
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.email);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+          SIGN UP FOR THE TLC NEWSLETTER.
+            <input type="text" value={this.state.email} onChange={this.handleChange} />
+          </label>
+          <button type="submit" >NEXT</button>
+        </form>
+      </div>
+    )
+  }
 }
