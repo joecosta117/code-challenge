e5c28e4a978a454e6df9168f8187f3ea65bd0987
joecosta117 <joecosta117@gmail.com>
2019-02-17 17:13:30 -0500

    styled the second form page for names

--- src/name-form.js
+++ src/name-form.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 const NameForm = ({ firstName, lastName, handleChange, handleSubmit }) => (
-  <div>
-    <form onSubmit={handleSubmit}>
-      <label>
-        <span className="formTitle">ALMOST DONE! PLEASE ENTER YOUR FIRST AND LAST NAME.</span>
-        <input type="text" name="firstName" value={firstName} placeholder="First Name" onChange={handleChange} />
-        <input type="text" name="lastName" value={lastName} placeholder="Last Name" onChange={handleChange} />
-      </label>
-      <button type="submit" >SIGN UP</button>
+  <div id="container">
+    <form onSubmit={handleSubmit} className="item">
+      <p id="formTitle">ALMOST DONE! PLEASE ENTER YOUR FIRST AND LAST NAME.</p>
+      <div id="nameForm">
+        <input type="text" name="firstName" value={firstName} placeholder="First Name" onChange={handleChange} id="nameField" />
+        <input type="text" name="lastName" value={lastName} placeholder="Last Name" onChange={handleChange} id="nameField" />
+        <button type="submit" className="finalButton">SIGN UP</button>
+      </div>
     </form>
   </div>
 )
