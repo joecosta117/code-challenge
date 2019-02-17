8cf093cfe81430d5f411865da32760760bd3b622
joecosta117 <joecosta117@gmail.com>
2019-02-17 16:11:50 -0500

    styling the mobile part

--- src/email-form.js
+++ src/email-form.js
@@ -1,19 +1,18 @@
 import React from 'react';
 
 const EmailForm = ({ email, emailDone, checked, handleChange, handleSubmit }) => (
-    <div>
-    <span className="
-    formTitle item">SIGN UP FOR THE TLC NEWSLETTER</span>
-      <form onSubmit={handleSubmit} className="item">
-        <div id="inputForm">
-          <input type="text" name="email" value={email} placeholder="enter email address" className="formField" onChange={handleChange} />
-          <button type="submit" className="button">NEXT</button>
-        </div>
-      </form>
-      <form onSubmit={handleSubmit} className="item" id="checkAgreement">
-          <input onChange={handleChange} type="checkbox" checked={checked} />
-          <label className="agreement">I agree to receive information from Discovery Communications in accordance with the following <u>Private Policy</u></label>
+    <div id="container">
+      <p id="formTitle" className="item" >SIGN UP FOR THE TLC NEWSLETTER</p>
+        <form onSubmit={handleSubmit} className="item">
+          <div id="inputForm">
+            <input type="text" name="email" value={email} placeholder="enter email address" className="formField" onChange={handleChange} />
+            <button type="submit" className="button">NEXT</button>
+          </div>
         </form>
+        <form onSubmit={handleSubmit} className="item" id="checkAgreement">
+            <input onChange={handleChange} type="checkbox" checked={checked} />
+            <label className="agreement">I agree to receive information from Discovery Communications in accordance with the following <u>Private Policy</u></label>
+          </form>
     </div>
 )
 
---
+++ src/main.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import FormContainer from './form-container'
+
+const Main = () => (
+  <div id="mainPage">
+    <div id="otherContent">
+      Hello World!
+    </div>
+    <FormContainer />
+  </div>
+)
+
+export default Main
