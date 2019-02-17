0a741a69fd34ce5d8c06e33b4d3c9010e42c047e
joecosta117 <joecosta117@gmail.com>
2019-02-17 17:35:57 -0500

    finished styling for congrats part of the footer

--- src/congrats-form.js
+++ src/congrats-form.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const CongratsForm = () => (
-  <div>
-    <h2>Thank You For Signing Up!</h2>
-    <span>Look out for the latest news on your favorite shows.</span>
+  <div id="congratsContainer">
+    <h2 id="congratsTitle">Thank You For Signing Up!</h2>
+    <p id="minorText">Look out for the latest news on your favorite shows.</p>
   </div>
 )
 
